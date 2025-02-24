1cac3385bbb849fc261ba37a84f35a9a9ae9ca8a
BALONDOJaymart <balondojaymart_bsit@plmun.edu.ph>
2025-02-24 14:44:55 +0800

    Update nested_for_loop_multiplication_table.py

--- nested_for_loop_multiplication_table.py
+++ nested_for_loop_multiplication_table.py
@@ -1,9 +1,5 @@
 for i in range(1, 6):
-        for j in range(1, 6):  # Indent this loop!
-                product = i * j  # Indent this line!
-                print(f"{product:4}", end="")  # Indent this line!
-<<<<<<< HEAD
-        print() # Indent this line!
-=======
-        print() # Indent this line!
->>>>>>> f1636a99bae2a1b65be52978eb77c71e511113f0
+    for j in range(1, 6): 
+        product = i * j 
+        print(f"{product:4}", end="")
+    print()  # Ensure this is correctly indented
